00d543892f28e1bf3001cd7750033d265603824e
Amogh Saxena <amoghsaxena24@gmail.com>
2023-03-17 00:45:04 +0530

    First Commit

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -12,293 +12,175 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size.height;
     return SafeArea(
       child: Scaffold(
-        // r
-        appBar: AppBar(
-          backgroundColor: Colors.transparent,
-          elevation: 0,
-          leading: GestureDetector(
-            onTap: () {
-              DrawerFb1();
-            },
-            child: const Icon(
-              Icons.menu,
-              color: Colors.black,
-              size: 30,
-            ),
-          ),
-        ),
-        body: Row(
-          children: [
-            Column(
-              children: [
-                GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const scanPage(),
-                      ),
-                    );
-                  },
-                  child: SizedBox(
-                      height: size * 0.43,
-                      width: size * 0.228,
-                      child: Column(
-                        children: [
-                          Expanded(
-                            child:Container()),
-                          const Icon(
-                            Icons.fullscreen,
-                            color: Colors.orange,
-                            size: 70,
-                          ),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          const Text("Scan", style: TextStyle(fontSize: 25)),
-                          Expanded(
-                            child:Container()),
-                        ],
-                      )),
-                ),
-                GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const settingsPage()));
-                  },
-                  child: SizedBox(
-                    height: size * 0.43,
-                    width: size * 0.228,
-                    child: Column(
-                        children: [
-                          Expanded(
-                            child:Container()),
-                          const Icon(
-                            Icons.settings,
-                            color: Colors.lightBlue,
-                            size: 70,
-                          ),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          const Text("Settings", style: TextStyle(fontSize: 25)),
-                          Expanded(
-                            child:Container()),
-                        ],
-                      )
-                     
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: size,
-              width: 2,
-            ),
-            Column(
-              children: [
-                SizedBox(
-                  height: size * 0.43,
-                  width: size * 0.23,
-                  child: GestureDetector(
-                    onTap: () {
-                      
-                    },
-                    child: Column(
-                        children: [
-                          Expanded(
-                            child:Container()),
-                          const Icon(
-                            Icons.save_outlined,
-                            color: Colors.red,
-                            size: 70,
-                          ),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          const Text("My Mocs", style: TextStyle(fontSize: 25)),
-                          Expanded(
-                            child:Container()),
-                        ],
-                      )
-                  ),
-                ),
-                SizedBox(
-                  height: size * 0.43,
-                  width: size * 0.23,
-                  child: GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const sharePage(),
-                        ),
-                      );
-                    },
-                    child: Column(
-                        children: [
-                          Expanded(
-                            child:Container()),
-                          const Icon(
-                            Icons.ios_share_rounded,
-                            color: Colors.green,
-                            size: 70,
-                          ),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          const Text("Share", style: TextStyle(fontSize: 25)),
-                          Expanded(
-                            child:Container()),
-                        ],
-                      )
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-
-
-//Drawer
-class DrawerFb1 extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-
-    return Drawer(
-        child: Material(
-          color: const Color(0xff4338CA),
-          child: ListView(
-            children: <Widget>[
-              Container(
-                padding: const EdgeInsets.all(15.0),
-                child: Column(
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.only(
+                    top: 4.0, left: 8.0, right: 8.0, bottom: 4.0),
+                child: Row(
                   children: [
-                    const SizedBox(height: 12),
-                    const SearchFieldDrawer(),
-                    const SizedBox(height: 12),
-                    MenuItem(
-                      text: 'Friends',
-                      icon: Icons.people,
-                      onClicked: () => selectedItem(context, 0),
-                    ),
-                    const SizedBox(height: 5),
-                    MenuItem(
-                      text: 'Liked Photos',
-                      icon: Icons.favorite_border,
-                      onClicked: () => selectedItem(context, 1),
-                    ),
-                    const SizedBox(height: 5),
-                    MenuItem(
-                      text: 'Workflow',
-                      icon: Icons.workspaces_outline,
-                      onClicked: () => selectedItem(context, 2),
-                    ),
-                    const SizedBox(height: 5),
-                    MenuItem(
-                      text: 'Updates',
-                      icon: Icons.update,
-                      onClicked: () => selectedItem(context, 3),
-                    ),
-                    const SizedBox(height: 8),
-                    const Divider(color: Colors.white70),
-                    const SizedBox(height: 8),
-                    MenuItem(
-                      text: 'Notifications',
-                      icon: Icons.notifications_outlined,
-                      onClicked: () => selectedItem(context, 5),
-                    ),
-                    MenuItem(
-                      text: 'Settings',
-                      icon: Icons.settings,
-                      onClicked: () => selectedItem(context, 6),
-                    ),
+                    GestureDetector(
+                      onTap: (){
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const settingsPage()));
+                      },
+                      child: Icon(Icons.menu, size: 40)),
+                    Expanded(child: Container()),
+                    Container(
+                        padding: EdgeInsets.only(top: 4.0),
+                        height: 45,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Image.asset(
+                          'assets/brick.png',
+                          fit: BoxFit.cover,
+                        ))
                   ],
                 ),
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Column(
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const scanPage(),
+                            ),
+                          );
+                        },
+                        child: Container(
+                          padding: EdgeInsets.only(left: 5, right: 5),
+                          color: Colors.orange.shade300,
+                            height: size * 0.44,
+                            width: size * 0.228,
+                            child: Column(
+                              children: [
+                                Expanded(child: Container()),
+                                 Icon(
+                                  Icons.fullscreen,
+                                  color: Colors.grey.shade800,
+                                  size: 70,
+                                ),
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                const Text("Scan my Bricks",
+                                textAlign: TextAlign.center,
+                                    style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold)),
+                                Expanded(child: Container()),
+                              ],
+                            )),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          
+                        },
+                        child: Container(
+                            color: Colors.lightBlue,
+                            height: size * 0.44,
+                            width: size * 0.228,
+                            child: Column(
+                              
+                              children: [
+                                Expanded(child: Container()),
+                                 Icon(
+                                  Icons.shopping_bag_outlined,
+                                  color: Colors.grey.shade800,
+                                  size: 70,
+                                ),
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                const Text("Purchase New Bricks",
+                                textAlign: TextAlign.center,
+                                    style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold)),
+                                Expanded(child: Container()),
+                              ],
+                            )),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: size,
+                    width: 2,
+                  ),
+                  Column(
+                    children: [
+                      Container(
+                        color: Colors.red.shade300,
+                        height: size * 0.44,
+                        width: size * 0.23,
+                        child: GestureDetector(
+                            onTap: () {},
+                            child: Column(
+                              children: [
+                                Expanded(child: Container()),
+                                Icon(
+                                  Icons.save_outlined,
+                                  color: Colors.grey.shade800,
+                                  size: 70,
+                                ),
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                const Text("My Mocs",
+                                    style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold)),
+                                Expanded(child: Container()),
+                              ],
+                            )),
+                      ),
+                      Container(
+                        color: Color.fromARGB(255, 70, 217, 77),
+                        height: size * 0.44,
+                        width: size * 0.23,
+                        child: GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const sharePage(),
+                                ),
+                              );
+                            },
+                            child: Column(
+                              children: [
+                                Expanded(child: Container()),
+                                 Icon(
+                                  Icons.storage_rounded,
+                                  color: Colors.grey.shade800,
+                                  size: 70,
+                                ),
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                const Text("My Scanned Brick",
+                                textAlign: TextAlign.center,
+                                    style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold)),
+                                Expanded(child: Container()),
+                              ],
+                            )),
+                      ),
+                    ],
+                  ),
+                ],
               ),
             ],
           ),
         ),
-    );
-  }
-
-
-
-  void selectedItem(BuildContext context, int index) {
-    Navigator.of(context).pop();
-    switch (index) {
-      case 0:
-        Navigator.of(context).push(MaterialPageRoute(
-          builder: (context) => const Scaffold(), // Page 1
-        ));
-        break;
-      case 1:
-        Navigator.of(context).push(MaterialPageRoute(
-          builder: (context) => const Scaffold(), // Page 2
-        ));
-        break;
-    }
-  }
-}
-class MenuItem extends StatelessWidget {
-  final String text;
-  final IconData icon;
-  final VoidCallback? onClicked;
-  
-  const MenuItem({required this.text,
-    required this.icon,
-    this.onClicked,Key? key, }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    final color = Colors.white;
-    final hoverColor = Colors.white70;
-
-    return ListTile(
-      leading: Icon(icon, color: color),
-      title: Text(text, style: TextStyle(color: color)),
-      hoverColor: hoverColor,
-      onTap: onClicked,
-    );
-  }
-}
-class SearchFieldDrawer extends StatelessWidget {
-  const SearchFieldDrawer({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    final color = Colors.white;
-
-    return TextField(
-      style: TextStyle(color: color,fontSize: 14),
-      decoration: InputDecoration(
-        contentPadding: const EdgeInsets.symmetric(horizontal: 5, vertical: 5),
-        hintText: 'Search',
-        hintStyle: TextStyle(color: color),
-        prefixIcon: Icon(Icons.search, color: color,size: 20,),
-        filled: true,
-        fillColor: Colors.white12,
-        enabledBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(15),
-          borderSide: BorderSide(color: color.withOpacity(0.7)),
-        ),
-        focusedBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(15),
-          borderSide: BorderSide(color: color.withOpacity(0.7)),
-        ),
       ),
     );
-  }}
+  }
+}
